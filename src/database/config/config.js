731afd2ead49f322c30726e484f209143b64c40b
Franckfer <francodel4to@gmail.com>
2021-11-18 19:44:11 -0300

    efggbgjjgv

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   "production": {
     "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
+    "password": null,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "mysql",
